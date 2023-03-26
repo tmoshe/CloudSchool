3e797c54be24003b538498ff2e6314bc2b6165a7
tmoshe <64310013+tmoshe@users.noreply.github.com>
2023-03-26 18:47:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
 #from flask_restful import Api
 #from db import db
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Hello, Flask112!!!"
-    
+
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0", port=4000 , debug=True)
+
