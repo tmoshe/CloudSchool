72e4381a44d0b2d7a25f1273625e0c5d57980ff5
tmoshe <tmoshe@wgmail.com>
2023-03-27 12:15:06 +0300

    Commit check

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
 #from flask_restful import Api
 #from db import db
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask112!!!"
-    
+    return "Hello, Flask1123!!!"
+
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0", port=4000 , debug=True)
